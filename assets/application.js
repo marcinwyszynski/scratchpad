f9a1c8311a57215818fe17e61d188a98cd725967
Tyler Davis <tydavis@users.noreply.github.com>
2017-06-27 11:14:38 -0700

    File Save function

--- assets/application.js
+++ assets/application.js
@@ -1,11 +1,10 @@
 (function() {
-
   // Turn a date element into a string formatted like YYYY-MM-DD.
   var dateToHash = function(date) {
     var month = '' + (date.getMonth() + 1);
     var day = '' + date.getUTCDate();
     if (month.length == 1) month = '0' + month;
-    if (day.length == 1) month = '0' + day;
+    if (day.length == 1) day = '0' + day;
     return [date.getFullYear(), month, day].join('-');
   };
 
@@ -22,6 +21,18 @@
     window.document.title = documentKey + ' | Scratchpad';
   };
 
+  // Save editor content to a file using the filesaver.js cross-browser API.
+  // If the document key has what looks like an extension, use it. Otherwise
+  // default to '.txt'.
+  var saveToFile = function() {
+    var keyParts = documentKey.split('.');
+    var extension = keyParts.length == 2 ? keyParts[1] : 'txt';
+    var blob = new Blob([editor.getValue()], {
+      type: 'text/plain;charset=utf-8'
+    });
+    saveAs(blob, [documentKey, extension].join('.'));
+  };
+
   // Fiat lux.
   window.addEventListener('load', function() {
     getDocumentKeyFromUrl();
@@ -33,13 +44,14 @@
     editor = CodeMirror(document.body, {
       lineNumbers: true,
       lineWrapping: true,
-      value: localStorage[documentKey] || '',
+      value: localStorage[documentKey] || ''
     });
 
     // Save changes as they happen in the editor to the local storage.
     editor.on('change', function() {
       localStorage[documentKey] = editor.getValue();
     });
+    document.querySelector('#saveToFile').addEventListener('click', saveToFile);
   });
 
   // When the hash is changed, pull and display the corresponding data.
